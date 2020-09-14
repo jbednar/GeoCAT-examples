5b43bb00d3c360bc146981fa36d8977154863b79
anissa111 <anissazacharias@gmail.com>
2020-09-14 09:33:25 -0600

    Plotting test

--- Plots/Regression/NCL_regress_1a.py
+++ Plots/Regression/NCL_regress_1a.py
@@ -12,8 +12,17 @@ See following URLs to see the reproduced NCL plot & script:
 
 ###############################################################################
 # Import packages
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 import geocat.datafiles as gdf
 from geocat.viz import util as gvutil
+
+###############################################################################
+# Create data
+
+# Data from original NCL file
+x =  np.array([1190, 1455, 1550, 1730, 1745, 1770, 1900, 1920, 1960, 2295, 2335, 2490, 2720, 2710, 2530, 2900, 2760, 3010], dtype=float)
+y =  np.array([1115, 1425, 1515, 1795, 1715, 1710, 1830, 1920, 1970, 2300, 2280, 2520, 2630, 2740, 2390, 2800, 2630, 2970], dtype=float)
+
