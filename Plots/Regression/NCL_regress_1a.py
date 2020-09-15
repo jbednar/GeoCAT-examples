5f7812613b6ad4aff2c93e45c8cb1790eecc6726
anissa111 <anissazacharias@gmail.com>
2020-09-15 09:37:52 -0600

    First regression plot created

--- Plots/Regression/NCL_regress_1a.py
+++ Plots/Regression/NCL_regress_1a.py
@@ -26,3 +26,52 @@ from geocat.viz import util as gvutil
 x =  np.array([1190, 1455, 1550, 1730, 1745, 1770, 1900, 1920, 1960, 2295, 2335, 2490, 2720, 2710, 2530, 2900, 2760, 3010], dtype=float)
 y =  np.array([1115, 1425, 1515, 1795, 1715, 1710, 1830, 1920, 1970, 2300, 2280, 2520, 2630, 2740, 2390, 2800, 2630, 2970], dtype=float)
 
+###############################################################################
+# First regression plot
+
+# Use numpy's polyfit to calculate a linear regression
+# This function outputs [a, b], where y = ax + b in the regression fit
+[a, b] = np.polyfit(x, y, 1)
+
+# Now that we have a y = ax + b, we can evaluate this model over a range,
+# which will give us points to plot
+x_regress = range(int(min(x)), int(max(x)))
+y_regress = a * x_regress + b
+
+# Generate figure (set its size (width, height) in inches) and axes
+plt.figure(figsize=(6, 6))
+ax = plt.gca()
+
+# Plot original data
+# Note that the s parameter sets the size of the markers in pts
+s = plt.scatter(x, y, color='black', s=20)
+
+# Plot regression
+plt.plot(x_regress, y_regress, color='black')
+
+# Use geocat.viz.util convenience function to set axes parameters
+gvutil.set_axes_limits_and_ticks(ax,
+                                 xlim=(1190, 3010),
+                                 xticks=np.arange(1200, 3010, 400),
+                                 ylim=(900, 3000),
+                                 yticks=np.arange(900, 3001, 300))
+
+# Use geocat.viz.util convenience function to add minor and major tick lines
+gvutil.add_major_minor_ticks(ax,
+                             x_minor_per_major=4,
+                             y_minor_per_major=3,
+                             labelsize=12)
+
+# Use geocat.viz.util convenience function to set titles and labels without calling several matplotlib functions
+gvutil.set_titles_and_labels(ax,
+                             maintitle="Brownlee Data",
+                             xlabel="X",
+                             ylabel="Y")
+
+# Set clip on to false to allow plots to overlap axes instead of being clipped
+s.set_clip_on(False)
+
+# Show plot
+plt.tight_layout()
+plt.show()
+
